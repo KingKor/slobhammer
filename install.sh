5c9601504459464126e8e1f719a2f447b417c8af
KingKor <lulzatyourface@tuta.io>
2016-05-16 18:24:50 -0400

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,5 @@
+sudo chmod -R 755 slob.sh slob2.sh slob3.sh slobloop.sh
+cd ~
+echo 'alias tor1="/usr/sbin/tor -f ~/Slobhammer/torrc2"' >> .bash_aliases
+echo 'alias tor2="/usr/sbin/tor -f ~/Slobhammer/torrc3"' >> .bash_aliases
+source ~/.bash_aliases
