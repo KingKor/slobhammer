f50ae26293e64ed24a4de36da2806ffcff8ac107
KingKor <lulzatyourface@tuta.io>
2016-05-25 17:31:57 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 sudo chmod -R 755 slob.sh slob2.sh slob3.sh slobloop.sh
 cd ~
-echo 'alias tor1="/usr/sbin/tor -f ~/Slobhammer/torrc2"' >> .bash_aliases
-echo 'alias tor2="/usr/sbin/tor -f ~/Slobhammer/torrc3"' >> .bash_aliases
+echo 'alias tor1="/usr/sbin/tor -f ~/sexploits/slobhammer/torrc2"' >> .bash_aliases
+echo 'alias tor2="/usr/sbin/tor -f ~/sexploits/slobhammer/torrc3"' >> .bash_aliases
 source ~/.bash_aliases
